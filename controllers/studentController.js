727bcfe27c3d630749db8139073222de2e9f6482
Kenny Hung (he) <khung@instructors.2u.com>
2023-02-02 20:48:47 +0000

    Average aggregate function

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -10,20 +10,24 @@ const headCount = async () => {
 }
 
 // Execute the aggregate method on the Student model and calculate the overall grade by using the $avg operator
-const grade = async (studentId) =>
-  Student.aggregate([
-    // TODO: Ensure we include only the student who can match the given ObjectId using the $match operator
+const grade = async (studentId) => {
+  const averageObject = await Student.aggregate([
     {
-      // Your code here
+      $match: { _id: ObjectId(studentId) }
     },
     {
       $unwind: '$assignments',
     },
     // TODO: Group information for the student with the given ObjectId alongside an overall grade calculated using the $avg operator
     {
-      // Your code here
-    },
+      $group: {
+        _id: 'hello',
+        average: { $avg: '$assignments.score' }
+      }
+    }
   ]);
+  return averageObject[0].average
+}
 
 module.exports = {
   // Get all students
